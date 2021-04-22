492b6b64a30783c2e21e0320921e5348502e42bf
ParkJooHyeong <67997760+ParkJooHyeong@users.noreply.github.com>
2021-04-22 14:45:52 +0900

    Manager 프로그램 작성.

---
+++ EquipManager/EquipManager.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace EquipManager
+{
+    partial class EquipManager
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tbMonitor = new System.Windows.Forms.TextBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_Start = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_End = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menu_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 407);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(627, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(627, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(627, 378);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(619, 352);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.splitContainer1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(619, 352);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Environment";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.tbPort);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tbMonitor);
+            this.splitContainer1.Size = new System.Drawing.Size(619, 352);
+            this.splitContainer1.SplitterDistance = 234;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tbMonitor
+            // 
+            this.tbMonitor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMonitor.Location = new System.Drawing.Point(3, 0);
+            this.tbMonitor.Multiline = true;
+            this.tbMonitor.Name = "tbMonitor";
+            this.tbMonitor.Size = new System.Drawing.Size(375, 352);
+            this.tbMonitor.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_Start,
+            this.menu_End,
+            this.toolStripMenuItem1,
+            this.menu_Exit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // menu_Start
+            // 
+            this.menu_Start.Name = "menu_Start";
+            this.menu_Start.Size = new System.Drawing.Size(180, 22);
+            this.menu_Start.Text = "Start";
+            this.menu_Start.Click += new System.EventHandler(this.menu_Start_Click);
+            // 
+            // menu_End
+            // 
+            this.menu_End.Name = "menu_End";
+            this.menu_End.Size = new System.Drawing.Size(180, 22);
+            this.menu_End.Text = "End";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menu_Exit
+            // 
+            this.menu_Exit.Name = "menu_Exit";
+            this.menu_Exit.Size = new System.Drawing.Size(180, 22);
+            this.menu_Exit.Text = "Exit Program";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(143, 49);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(60, 21);
+            this.tbPort.TabIndex = 0;
+            this.tbPort.Text = "9001";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server TCP Port";
+            // 
+            // EquipManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 429);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "EquipManager";
+            this.Text = "Equipment Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EquipManager_FormClosing);
+            this.Load += new System.EventHandler(this.EquipManager_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox tbMonitor;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menu_Start;
+        private System.Windows.Forms.ToolStripMenuItem menu_End;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menu_Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPort;
+    }
+}
+
---
+++ EquipManager/EquipManager.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using jsLibrary;
+
+namespace EquipManager
+{
+    public partial class EquipManager : Form
+    {
+        public EquipManager()
+        {
+            InitializeComponent();
+        }
+
+        Socket sock = null;
+        TcpListener listener = null;
+        Thread serverThread = null;
+        Thread readThread = null;
+        List<Socket> sock_list = new List<Socket>();
+        delegate void CallBackAddText(string str);
+        void AddText(string str)
+        {
+            if (tbMonitor.InvokeRequired)
+            {
+                CallBackAddText cb = new CallBackAddText(AddText);
+                Invoke(cb, new object[] { str });
+            }
+            else
+            {
+                tbMonitor.AppendText(str);
+            }
+        }
+
+        private void menu_Start_Click(object sender, EventArgs e)
+        {
+            if (listener == null) listener = new TcpListener(int.Parse(tbPort.Text));
+            else listener.Stop();
+            listener.Start();
+
+            if (serverThread != null) serverThread.Abort();
+            serverThread = new Thread(ServerProcess);
+            serverThread.Start();
+
+            if (readThread != null) readThread.Abort();
+            readThread = new Thread(ReadProcess);
+            readThread.Start();
+
+        }
+        void ServerProcess()    // Socket들을 기다리는 Thraead.
+        {
+            while (true)
+            {
+                if (listener.Pending())
+                {
+                    Socket sockT = listener.AcceptSocket();
+                    sock_list.Add(sockT);
+                }
+                Thread.Sleep(100);
+            }
+
+        }
+        void ReadProcess()  //각 통신 정보를 가져올 쓰레드
+        {
+            while (true)
+            {
+                for(int i = 0; i < sock_list.Count; i++)
+                {
+                    Socket lsock = sock_list[i];
+                    if (IsAlive(lsock) == false) continue;
+                    if (lsock.Available > 0)
+                    {
+                        byte[] bArr = new byte[lsock.Available];
+                        lsock.Receive(bArr);
+                        ReadPro(lsock, bArr);
+                    }
+                }
+                Thread.Sleep(100);
+            }
+
+        }
+        void ReadPro(Socket ss, byte[] arr)
+        {
+            AddText(ss.RemoteEndPoint.ToString()+"> ");
+            AddText(Encoding.Default.GetString(arr)+"\r\n");
+
+        }
+        bool IsAlive(Socket sc) // Socket이 유효한지 판별.
+        {
+            if (sc == null) return false;
+            if (sc.Connected == false) return false;
+            if (sc.Poll(1000, SelectMode.SelectRead) && sc.Available > 0) return false;
+
+            try
+            {
+                sc.Send(new byte[1], 0, SocketFlags.OutOfBand); //제대로 연결되있는지 확인하기 위한 전송 메소드 실행.
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+        iniFile ini = new iniFile(".\\EquipManager.ini");
+        private void EquipManager_Load(object sender, EventArgs e)
+        {
+            tbPort.Text = ini.GetString("Connection", "Port", "8080");
+
+            int x1, x2, y1, y2;
+            x1 = int.Parse(ini.GetString("Form", "LocationX", "0"));
+            y1 = int.Parse(ini.GetString("Form", "LocationY", "0"));
+            this.Location = new Point(x1, y1);
+            x2 = int.Parse(ini.GetString("Form", "SizeX", "500"));
+            y2 = int.Parse(ini.GetString("Form", "SizeY", "500"));
+            this.Size = new Size(x2, y2);
+
+            tabControl1.SelectedIndex = int.Parse(ini.GetString("Form", "TabIndex", "1"));
+        }
+
+        private void EquipManager_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ini.SetString("Connection", "Port", tbPort.Text);
+
+            ini.SetString("Form", "LocationX", this.Location.X.ToString());
+            ini.SetString("Form", "LocationY", this.Location.Y.ToString());
+            ini.SetString("Form", "SizeX", this.Size.Width.ToString());
+            ini.SetString("Form", "SizeY", this.Size.Height.ToString());
+
+            ini.SetString("Form", "TabIndex", $"{ tabControl1.SelectedIndex}");
+
+            if (serverThread != null) serverThread.Abort();
+            if (readThread != null) readThread.Abort();
+
+        }
+    }
+}
